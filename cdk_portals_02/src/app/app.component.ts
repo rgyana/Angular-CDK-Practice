125d63ef97154f8dde5d08e6c7352fcba678fb99
Gyana <rgyanaranjan58@gmail.com>
2022-12-12 22:28:16 +0530

    second commit in portal 02

--- cdk_portals_02/src/app/app.component.ts
+++ cdk_portals_02/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, Injector, OnInit, ApplicationRef } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'cdk_portals_02';
+export class AppComponent implements OnInit{
+  private componentRef: any;
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private applicationRef : ApplicationRef
+  ) {}
+
+  ngOnInit(): void {
+      // locate and element that exist on page
+      const headerElement = document.querySelector('#pageHeader');
+
+      // locate component factory for component header
+      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(HeaderComponent);
+
+      // generate an instance of header component
+      this.componentRef = componentFactory.create(this.injector, [], headerElement);
+
+      // attach to the component to angular's component tree for dirty checking
+      this.applicationRef.attachView(this.componentRef.hostView);
+  }
 }
